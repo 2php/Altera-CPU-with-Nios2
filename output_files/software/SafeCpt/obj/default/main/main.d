c79f98e6c466c2c3f164786ea40db93b3f71731f
hellobyn <obyn@qq.com>
2015-07-06 16:55:54 +0800

    实现了在MAX10中嵌入Nios2，并实现了流水灯实验。

---
+++ output_files/software/SafeCpt/obj/default/main/main.d
@@ -0,0 +1,8 @@
+obj/default/main/main.o: main/main.c main/../inc/sopc.h \
+ ../SafeCpt_bsp/system.h ../SafeCpt_bsp/linker.h
+
+main/../inc/sopc.h:
+
+../SafeCpt_bsp/system.h:
+
+../SafeCpt_bsp/linker.h:
